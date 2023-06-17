66c227cd304b1096739ba6598af2ba4b87de56ca
Faiyaz <faiyazansari0010@gmail.com>
2023-06-17 20:16:52 +0530

    Adding Input/Output

---
+++ Input Output/sum.cpp
@@ -0,0 +1,21 @@
+//To print sum of two numbers.
+#include<iostream>
+
+using namespace std;
+
+int main(){ 
+
+cout<<"Type your two numbers:\n";
+int amount1;
+cin>>amount1;
+
+int amount2;
+cin>>amount2;
+
+int sum=amount1+amount2;
+
+cout<<sum;
+
+return 0;
+
+}

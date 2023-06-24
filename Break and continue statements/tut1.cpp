25cc1817d93586c48b238ee4d91a3d573f335d6e
Faiyaz <faiyazansari0010@gmail.com>
2023-06-24 16:50:14 +0530

    Adding Break and Continue statements

---
+++ Break and continue statements/tut1.cpp
@@ -0,0 +1,21 @@
+//To print all numbers from 0 to 100 except those divisible by 3.
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+for(int num=0; num<=100; num++){
+
+    if(num%3==0){
+
+        continue;}
+
+        cout<<num<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
